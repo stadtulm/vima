e690410a803bd95755968157506aabbe4e4bd24c
Falko Pross <falko@widerstand.digital>
2023-08-11 14:53:40 +0200

    Update meta project stuff

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,29 @@
 module.exports = {
-  root: true,
   env: {
-    node: true
+    browser: true,
+    es2021: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: [
+    'vue'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
-    'no-empty': 'off'
+    'no-empty': 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/no-deprecated-v-bind-sync': 'off',
+    'vue/no-v-for-template-key-on-child': 'off',
+    'vue/no-deprecated-v-on-native-modifier': 'off'
   }
 }
