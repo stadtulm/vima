dcc1bbe826f9617af92828b3423afa598ca095bf
Falko Pross <falko@widerstand.digital>
2024-05-06 17:44:43 +0200

    fixup! Introduce nationality

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -69,6 +69,9 @@ module.exports = function (app) {
     language: {
       type: String
     },
+    nationality: {
+      type: String
+    },
     isActive: {
       type: Boolean,
       default: true
